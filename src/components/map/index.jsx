6d53b58216f55b3cd6a718d87a63de568b0d65c4
Vitor <v.augustosilva@gmail.com>
2020-01-18 02:08:24 -0300

    Add reverse geolocation in useCoordinates hook

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCoordinates } from "../../state/hooks/useCoordinates";
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker
+} from "react-google-maps";
 import styles from "./map.module.css";
 
 const Map = () => {
-  const google = window.google;
-
-  const [latitude, longitude] = useCoordinates({});
+  const [latitude, longitude, userAddress] = useCoordinates({});
+  const googleMapAPIKey = "AIzaSyBX1DZz2mY9XjFTFfPwhdZArN-AqQlt52E";
 
   function TestMap() {
     return (
       <GoogleMap
         defaultZoom={10}
-        defaultCenter={{ lat: -19.592079, lng: -46.940552 }}
-      />
+        defaultCenter={{ lat: -27.59691, lng: -48.54958 }}
+        center={{ lat: latitude, lng: longitude }}
+      >
+        {
+          <Marker
+            position={{ lat: latitude, lng: longitude }}
+            icon={{
+              url: "/pin.svg",
+              scaledSize: new window.google.maps.Size(30, 30)
+            }}
+          />
+        }
+      </GoogleMap>
     );
   }
 
   const WrappedMap = withScriptjs(withGoogleMap(TestMap));
-
-  const key = "AIzaSyBX1DZz2mY9XjFTFfPwhdZArN-AqQlt52E";
 
   return (
     <div>
       <div>google maps here</div>
       <div>{latitude}</div>
       <div>{longitude}</div>
+      <div>{userAddress}</div>
       <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${key}&v=3.exp&libraries=geometry,drawing,places`}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${googleMapAPIKey}&v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div className={styles.map} />}
         containerElement={<div className={styles.map} />}
         mapElement={<div className={styles.map} />}
