c5f00fb6a4a0d9fe24da8a946046cbe92801cb18
Vitor <v.augustosilva@gmail.com>
2020-02-12 18:56:25 -0300

    Add weather forecast information
    Before it was only current weather information

--- src/store/ducks/weather.jsx
+++ src/store/ducks/weather.jsx
@@ -1,5 +1,6 @@
 export const Types = {
-  SET: "weather/SET"
+  SET_CURRENT_WEATHER: "weather/SET_CURRENT_WEATHER",
+  SET_WEATHER_FORECAST: "weather/SET_WEATHER_FORECAST"
 };
 
 // --- reducers
@@ -8,8 +9,13 @@ const INITIAL_STATE = {};
 
 export default function weather(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case Types.SET:
-      return { weatherInfo: action.payload.weatherInfo };
+    case Types.SET_CURRENT_WEATHER:
+      console.log("SET_CURRENT_WEATHER");
+      return { ...state, currentWeather: action.payload.weatherInfo };
+    case Types.SET_WEATHER_FORECAST:
+      console.log("SET_WEATHER_FORECAST");
+
+      return { ...state, weatherForecast: action.payload.weatherInfo };
     default:
       return state;
   }
@@ -18,10 +24,16 @@ export default function weather(state = INITIAL_STATE, action) {
 // --- actions
 
 export const Creators = {
-  setWeather: weatherInfo => ({
-    type: Types.SET,
+  setCurrentWeather: currentWeather => ({
+    type: Types.SET_CURRENT_WEATHER,
     payload: {
-      weatherInfo: weatherInfo
+      currentWeather: currentWeather
+    }
+  }),
+  setWeatherForecast: weatherForecast => ({
+    type: Types.SET_WEATHER_FORECAST,
+    payload: {
+      weatherForecast: weatherForecast
     }
   })
 };
